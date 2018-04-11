1ff5410542ab2c576372bf1fce562794bdf68140
sdl83 <sdl83@cornell.edu>
2018-04-10 23:48:40 -0500

    mapping arduino sensor value to canvas position

--- public/client.js
+++ public/client.js
@@ -4,6 +4,8 @@ var socket = io();        // websocket to the server
 var previousPosition=[0,0]; // previous position to draw a line from
 var ctx = Sketch.create(); //Creating the drawing context
 var firstMessage=true;    // What the first message, to start on the first value
+var width;
+var height;
 
     ctx.container = document.getElementById( 'container' ); //reference drawing canvas
     ctx.autoclear= false; // making sure it stays
@@ -19,6 +21,15 @@ var firstMessage=true;    // What the first message, to start on the first value
     socket.on('new-pos', function(newPosition) { // handling new sensor values
 
       //TODO: Map the incoming 10-bit numbers to the height and width of the screen.
+      x_offset = 30;
+      y_offset = 30;
+
+      width = ctx.width - 2 * x_offset;
+      height = ctx.height - 2 * y_offset;
+
+      newPosition[0] = x_offset + newPosition[0] / 1023 * width;
+      newPosition[1] = height - (newPosition[1] / 1023 * height) + y_offset;
+
       // See https://github.com/soulwire/sketch.js/wiki/API for sketch references
 
       if(firstMessage){ // if its the first message store that value as previous
